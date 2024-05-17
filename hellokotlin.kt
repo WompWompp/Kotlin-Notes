c9b240c530468da7291ae8e2c21ae9a9a64b1881
Mustafa Efe Emeksiz <152984216+WompWompp@users.noreply.github.com>
2024-05-17 23:33:54 +0200

    Add files via upload

---
+++ hellokotlin.kt
@@ -0,0 +1,121 @@
+package com.example.myfirstproject
+
+//Arrays
+
+/*
+fun main() {
+
+   val myArray = arrayOf("cat","dog","bird","snake")
+   myArray.set(0,"Kot")
+   myArray[1] = "Pies"
+   println(myArray.get(0))
+   println(myArray.get(1))
+   val a:Int = 0
+   for (a in 0..3){
+      println(myArray.get(a))
+      val a = a + 1
+   }
+
+
+}
+ */
+/*
+fun main(){
+
+   val myArray = arrayOf(1,2,3,4,5)
+   println(myArray[3])
+
+}
+ */
+/*
+fun main(){
+   val myArray = arrayOf("cat",2,2.2,3,4,7)
+   println(myArray[2])
+   val arraySize = myArray.size
+   println(arraySize)
+}
+
+*/
+
+//Lists
+
+/*
+fun main() {
+   //arrayList
+   val myArray = ArrayList<String>()
+   myArray.add("Cat")
+   myArray.add("Dog")
+   println(myArray[0])
+   //arrayListOf
+   val myArrayListOf = arrayListOf<String>("a","b","c")
+   myArrayListOf.add("d")
+   println(myArrayListOf[2])
+   println(myArrayListOf[3])
+   //add to index
+   myArrayListOf.add(4,"e")
+   println(myArrayListOf[4])
+   myArrayListOf.add(0,"01")
+   println(myArrayListOf[0])
+   //set
+   myArrayListOf.set(0,"02")
+   println(myArrayListOf[0])
+   //any
+   val myAnyList = ArrayList<Any>()
+   myAnyList.add("a")
+   myAnyList.add(1)
+   myAnyList.add(true)
+   myAnyList.add(2.22)
+   println(myAnyList[0])
+   }
+*/
+
+//Sets
+// Don't count the same attribute
+/*
+fun main() {
+    //arrayof vs setof
+
+    val myArray = arrayOf(1,2,2,3,4)
+    println(myArray.size)
+    val mySet = setOf(1,2,2,2,2,2,2,2,2,2,2,3,3,3,4)
+    println(mySet.size)
+    //forEach
+    mySet.forEach{println(it)}
+    //hashset
+    val myHashSet = HashSet<String>()
+    myHashSet.add("Cat")
+    myHashSet.add("Cat")
+    println(myHashSet.size)
+
+}
+*/
+
+// Hashmap
+
+fun main(){
+    //key value logic
+
+    //val cityArray = arrayOf("ankara","istanbul")
+    //val degreeArray = arrayOf(25,27)
+    //println("${cityArray[0]} : ${degreeArray}")
+
+    //No index in hashmaps
+   // val myHash = HashMap<String,Int>()
+   // myHash["ankara"]=25
+    //myHash["istanbul"]=15
+
+    //println(myHash["istanbul"])
+   // println("Ankara: ${myHash["ankara"]}")
+   // println("istanbul: ${myHash["istanbul"]}")
+
+   // val myHashMap = hashMapOf<Int,String>(1 to "monday",2 to "tuesday",3 to "wednesday")
+    //myHashMap[4]="thursday"
+
+}
+
+
+
+
+
+
+
